b77a5c21d45a3ba93641944e21632a9faf9511bc
sekh_mahadi <sekh.mahadi2007cse@gmail.com>
2025-08-16 00:10:40 +0600

    Spliting code into funtions.

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,13 +1,17 @@
-const defaultResult = 0
+const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
+  const enteredNumber = getUserInput(); 
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescription);
 }
- currentResult = add(5, 6);
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+//  currentResult = add(5, 6);
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
-
-outputResult(currentResult, calculationDescription);
+// currentResult = (currentResult + 10) * 3 / 2 - 1;
+// let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+addBtn.addEventListener("click", add);
